5366a97f84770853ea027d91756719da29863583
Sagar Variya <77156364+Sagar-v4@users.noreply.github.com>
2022-06-23 05:34:13 +0530

    init

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,9 @@ const {deleteUser, updateUser, getUser, getUsers} = require("../controllers/user
 const router = require("express").Router();
 
 // TEST
-// router.get("/", (req, res) => {
-//     res.send("user route...");
-// })
+router.get("/", (req, res) => {
+    res.send("user route...");
+})
 
 // UPDATE
 router.put("/:id", updateUser);
